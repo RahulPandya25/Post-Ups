3d38e616207636b98607db794ffa32ca04f5271e
Rahul Pandya <rahul25796@gmail.com>
2020-07-01 16:13:58 -0400

    > adds comment to api.js

--- controller/api.js
+++ controller/api.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var router = express.Router();
 
+// ********************* //
+// base api call: '/api' //
+// ********************* //
+
 router.get("/", function (req, res, next) {
   res.send("api called!!");
 });
